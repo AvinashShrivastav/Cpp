0beb965fe26301526f3fb9d048135cee9e22274a
AvinashShrivastav <avinashshrivastavaofficial@gmail.com>
2023-05-17 22:09:45 +0530

    Add | Practice folder

---
+++ Cpp Practice Question/example1.cpp
@@ -0,0 +1,22 @@
+//Pointer to member of class
+
+#include <iostream>
+using namespace std;
+class A 
+    {
+        public:
+        int m;
+        void show();
+
+    };
+
+int A::*ip = &A :: m; 
+//A::* means pointer to member of A class
+//&A::m means address of m member of A class
+int main(){
+    A a;
+    cout << a.*ip<<endl;
+    cout << a.m<<endl;
+    ap = &a;
+    cout<<ap ->*ip;
+}
